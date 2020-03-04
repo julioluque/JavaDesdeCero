d4047c4fbe631b77103c7894a419a7dabc5938bc
Julio Luque <julio.a.luque@gmail.com>
2020-03-03 23:58:09 -0300

    Swing

---
+++ src/graficos/CreandoMarcos.java
@@ -0,0 +1,28 @@
+package graficos;
+
+import javax.swing.*;
+
+public class CreandoMarcos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		miMarco marco1 = new miMarco();
+		marco1.setVisible(true);
+		marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		marco1.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		
+	}
+
+}
+
+class miMarco extends JFrame{
+	
+	public miMarco() {
+
+		setSize(500, 300);
+		
+		
+	}
+	
+}
