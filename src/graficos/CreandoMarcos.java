59bc655f474e0debdf9bbf3668e52fbc51a29753
Julio Luque <julio.a.luque@gmail.com>
2020-03-04 00:10:38 -0300

    Ventana basica Java.swing

--- src/graficos/CreandoMarcos.java
+++ src/graficos/CreandoMarcos.java
@@ -1,5 +1,7 @@
 package graficos;
 
+import java.awt.Frame;
+
 import javax.swing.*;
 
 public class CreandoMarcos {
@@ -10,7 +12,6 @@ public class CreandoMarcos {
 		miMarco marco1 = new miMarco();
 		marco1.setVisible(true);
 		marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		marco1.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		
 	}
 
@@ -20,9 +21,15 @@ class miMarco extends JFrame{
 	
 	public miMarco() {
 
-		setSize(500, 300);
+//		setSize(500, 300);
+//		setLocation(500, 300);
 		
+		setBounds(300, 200, 550, 250);
+	
+//		setResizable(false);
+//		setExtendedState(Frame.MAXIMIZED_BOTH);
 		
+		setTitle("Mi ventana");
 	}
 	
 }
